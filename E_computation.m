2aed004e3401ccb33fd2b716673b7feff9a5dcb1
Nicolas <nicolas.clairis@protonmail.com>
2025-07-24 14:16:30 +0200

    adaptations for Atarget<Abase

--- E_computation.m
+++ E_computation.m
@@ -1,26 +1,44 @@
-function[E] = E_computation(Abase, Atarget, Amax)
-% [E] = E_computation(Abase, Atarget, Amax)
+function[E] = E_computation(Abase, Atarget, Amax, Amin)
+% [E] = E_computation(Abase, Atarget, Amax, Amin)
 %
 % INPUTS
 % Abase: numeric value or vector of A at baseline
 %
 % Atarget: numeric value or vector of target A to reach
 %
-% Amax: numeric value or vector of maximal A that one can attain
+% Amax: numeric value or vector of maximal A that one can attain (by
+% default it will be set up to 1)
+%
+% Amin: numeric value or vector of minimal A that one can attain (by
+% default it will be set up to 0, but this doesn't make sense in real
+% conditions)
 %
 % OUTPUTS
 % E: numeric value of E
 % 
 
+%% check inputs and define default values
+% Amax
+if ~exist('Amax','var') || isempty(Amax)
+    Amax = 1;
+end
+
+% Amin
+if ~exist('Amin','var') || isempty(Amin)
+    Amin = 0;
+end
+
 %% compute inputs for E
-Wtarget = abs(Atarget - Abase); % if you consider that lowering A is also a process requiring E
-% Wtarget = Atarget - Abase;
-Wcapacity = Amax - Abase;
+Aincrease = Atarget > Abase; % increase in arousal
+Adecrease = Atarget <= Abase; % decrease in arousal
+
+Wtarget = abs(Atarget - Abase); % distance between Atarget and Abase
+Wmax = (Amax - Abase).*Aincrease + (Abase - Amin).*Adecrease; % max possible work = increasing A to Amax (if arousal increase) or decreasing A to Amin (if arousal decrease)
 
 %% compute E
-E = Wtarget./Wcapacity;
+E = Wtarget./Wmax;
 
-%% constraint E to be positive
-E(E < 0) = 0;
+% %% constraint E to be positive
+% E(E < 0) = 0;
 
 end % function
